812124bf20e4cbca67ac544fe3630b3d0389e84c
Heshanka <heshanka123@gmail.com>
2020-11-10 04:01:27 +0300

    Create Screen.dart

---
+++ Screen.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class Screen extends StatefulWidget {
+
+  @override
+  _ScreenState createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+
+  Alignment editBtnAlign = Alignment(0.905, 0.954);
+  Alignment emotionBtnAlign = Alignment(0.905, 0.954);
+  bool fabClick = false;
+
+  void changeBtnAlign() {
+    if (fabClick)
+      setState(() {
+        editBtnAlign = Alignment(0.7, 0.7);
+        emotionBtnAlign = Alignment(0.4, 0.85);
+      });
+    else {
+      setState(() {
+        editBtnAlign = Alignment(0.905, 0.954);
+        emotionBtnAlign = Alignment(0.905, 0.954);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            Column(
+              children: [
+              ],
+            ),
+            AnimatedAlign(
+              alignment: editBtnAlign,
+              curve: Curves.easeOut,
+              duration: Duration(milliseconds: 300),
+              onEnd: () {
+                print("ANIMATION ENDED");
+              },
+              child: FloatingActionButton(
+                backgroundColor: Color(0xff89cf95),
+                foregroundColor: Colors.white,
+                child: Icon(Icons.edit),
+                onPressed: () {
+                  //titleFocusNode.requestFocus();
+                },
+              ),
+            ),
+            AnimatedAlign(
+              alignment: emotionBtnAlign,
+              curve: Curves.easeOut,
+              duration: Duration(milliseconds: 300),
+              onEnd: () {
+                print("ANIMATION ENDED");
+              },
+              child: FloatingActionButton(
+                backgroundColor: Color(0xffF4C2C2),
+                foregroundColor: Colors.white,
+                child: Icon(Icons.add_a_photo),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+        //todo what should this button do exactly?
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff89cff0),
+          foregroundColor: Colors.white,
+          child: Icon(
+            Icons.add,
+            size: 40,
+          ),
+          onPressed: () {
+            setState(() {
+              fabClick = !fabClick;
+            });
+            changeBtnAlign();
+          },
+        ),
+    );
+  }
+}
